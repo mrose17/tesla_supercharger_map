e8482c96766640f7bab5dcc6e97e10fa3ae35c7e
Eitan Rosenfeld <eitanr@cs.technion.ac.il>
2014-05-01 19:41:18 +0300

    Improved comments and tabs

--- webcontent/scripts/siteload/crawl_stalls.py
+++ webcontent/scripts/siteload/crawl_stalls.py
@@ -2,23 +2,30 @@ import sys
 import urllib
 from lxml.html import fromstring
 
+# Extract URLs from superchargers.txt
+# Use those URLs to crawl supercharger pages for stall numbers
+
 def main(argv):
-	#Extracts URLs from superchargers.txt to crawl supercharger pages for stall numbers
-	
+
     sc_file = open("superchargers.txt","rw")
     url_prefix = "url: http://www.teslamotors.com/supercharger/"
     lines = sc_file.readlines()
 
     station = ''
+    gps_string = ''
     for line in lines:
         line=line.rstrip('\n')
 
-		#Get station name
+        # Get station name
         if "name:" in line:
-			words = line.split(": ")
-			station = words[1]
-			
-		#Crawl supercharger webpage if it has an official page listed
+            words = line.split(": ")
+            station = words[1]
+
+        # Use GPS string to determine the location in data file to insert stall info
+        if "gps:" in line:
+            gps_string = line
+
+        # Crawl supercharger webpage if it has an official page listed
         if url_prefix in line:
             url = line.split(" ")[1]
             content = urllib.urlopen(url).read()
@@ -27,10 +34,12 @@ def main(argv):
             if elements > 1:
                 element = elements[0]
                 inner_text = element.text_content()
-				#Change inner text from unicode to ascii 
+
+                # Change inner text from unicode to ascii 
                 words = inner_text.encode('ascii','ignore')
-				#Format is "Charging Stalls – <INTEGER>" 
-				#Extract integer from words
+
+                # Format is "Charging Stalls – <INTEGER>" 
+		# Extract integer from words
                 int_array = [int(s) for s in words.split() if s.isdigit()]
                 num_stalls = int_array[0]
                 print "%s: %d" % (station, num_stalls)
