5db4f83cb3fc8e88ce6bad1e6c868e04314fa36c
Eitan Rosenfeld <eitanr@cs.technion.ac.il>
2014-05-05 00:26:16 +0300

    Works, and prints supercharger stall info as it gets retrieved

--- webcontent/scripts/siteload/crawl_stalls.py
+++ webcontent/scripts/siteload/crawl_stalls.py
@@ -2,49 +2,54 @@ import sys
 import urllib
 from lxml.html import fromstring
 
-# Extract URLs from superchargers.txt
-# Use those URLs to crawl supercharger pages for stall numbers
+# Uses URLs from superchargers.txt to crawl supercharger websites for stall count
+# This script assumes that URLs are listed BEFORE the dateOpened line
 
 def main(argv):
-
-    sc_file = open("superchargers.txt","rw")
     url_prefix = "url: http://www.teslamotors.com/supercharger/"
-    lines = sc_file.readlines()
+    readonly_sc_file = open("superchargers.txt","r")
+    sc_lines = readonly_sc_file.readlines()
+    readonly_sc_file.close()
 
+    sc_file = open("superchargers.txt","w+")
     station = ''
-    elevation = ''
-    for line in lines:
-        raw_line=line.rstrip('\n')
+    num_stalls = -1 # -1 means stall count for station has not been retrieved
 
-        # Get station name
+    for sc_line in sc_lines:
+        if "numStalls:" not in sc_line:
+            sc_file.write(sc_line)
+
+        # Get supercharger name
+        raw_line = sc_line.rstrip('\n')
         if "name:" in raw_line:
+            num_stalls = -1 # new station entry
             words = raw_line.split(": ")
             station = words[1]
 
-        # Use 'elevation' string for the future location in the data file for stall info
-        if "elevation:" in line:
-            elevation = line
-
-        # Crawl supercharger webpage if it has an official page listed
+        # Crawl the supercharger's official webpage on teslamotors.com
         if url_prefix in raw_line:
             url = raw_line.split(" ")[1]
             content = urllib.urlopen(url).read()
             doc = fromstring(content)
             elements = doc.find_class('stalls')
-            if elements > 1:
+            if len(elements) > 0: # CSS class 'stalls' has been found
                 element = elements[0]
                 inner_text = element.text_content()
 
-                # Change inner text from unicode to ascii 
-                words = inner_text.encode('ascii','ignore')
+                # Format is "Charging Stalls \xe2 NUM_STALLS"
+                # Change inner text from unicode to ascii
+                encoded_line = inner_text.encode('ascii','ignore')
 
-                # Format is "Charging Stalls – <INTEGER>" 
-		# Extract integer from words
-                int_array = [int(s) for s in words.split() if s.isdigit()]
+                # Extract integer from encoded_line
+                int_array = [int(s) for s in encoded_line.split() if s.isdigit()]
                 num_stalls = int_array[0]
                 print "%s: %d" % (station, num_stalls)
-            else:
-                print "%s: no data for stalls " % (station)
+
+        if "dateOpened:" in sc_line and num_stalls > -1:
+            sc_file.write('numStalls: {0}\n'.format(num_stalls))
+
+
+    sc_file.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
