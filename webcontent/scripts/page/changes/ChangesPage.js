cc734c7c39c6ad4ac7c4c2a0ec56c09dab47c452
Keith David Winkler <keithdwinkler@gmail.com>
2014-05-18 15:33:45 -0500

    data service: use service for changelog

--- webcontent/scripts/page/changes/ChangesPage.js
+++ webcontent/scripts/page/changes/ChangesPage.js
@@ -12,25 +12,18 @@ define([], function () {
 
     ChangesPage.prototype.onPageShow = function () {
         if (!this.changesPage.data(ChangesPage.INIT_PROP)) {
-            jQuery.get("changelog.txt", "", jQuery.proxy(this.handleTextBlock, this), "html");
+            jQuery.getJSON("http://localhost:8080/supercharge/allChanges", "", jQuery.proxy(this.handleChangesJson, this), "html");
         }
     };
 
-    ChangesPage.prototype.handleTextBlock = function (textBlock) {
-        var linesArray = textBlock.split("\n");
-        this.handleLines(linesArray);
-        this.changesPage.data(ChangesPage.INIT_PROP, true);
-    };
-
-    ChangesPage.prototype.handleLines = function (linesArray) {
+    ChangesPage.prototype.handleChangesJson = function (changeArray) {
         var v = this;
-        jQuery.each(linesArray, function (index, line) {
-            if (line.trim().length > 0) {
-                var parts = line.trim().split("||");
-                var cssClass = (/live:/.test(parts[1])) ? 'success' : '';
-                v.changesTable.append("<tr class='" + cssClass + "'><td>" + parts[0] + "</td><td>" + parts[1] + "</td></tr>");
-            }
+        jQuery.each(changeArray, function (index, change) {
+            var cssClass = (change.changeType === 'LIVE') ? 'success' : '';
+            v.changesTable.append("<tr class='" + cssClass + "'><td>" + change.dateFormatted + "</td><td>" + change.description + "</td></tr>");
         });
+        this.changesPage.data(ChangesPage.INIT_PROP, true);
+
     };
 
 
